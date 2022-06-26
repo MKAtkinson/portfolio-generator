7e5899e61875c289e69f6f6d210d8c793437d37e
MKAtkinson <maxatkinson@ymail.com>
2022-06-26 16:14:20 -0500

    Inquirer module installed and working

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2,);
-const [name, github] = profileDataArgs
-
-
-
-fs.writeFile('./index.html',generatePage(name, github), err =>{
-  if (err) throw new Error(err)
-
-  console.log('Portfolio complete! Checkout index.html to see output!')
-})
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+import inquirer from "inquirer";
+const response = await inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
